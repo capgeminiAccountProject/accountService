fb8855e45d3c6df7bbb6238ce0bdd2ff99a13a4b
Jain <aakanksha.jain@capgemini.com>
2019-06-06 11:18:13 -0500

    Lombok, Exception Handling

---
+++ src/main/java/com/cap/CapAccountApplication.java
@@ -0,0 +1,13 @@
+package com.cap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapAccountApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapAccountApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/cap/dto/AccountRegistration.java
@@ -0,0 +1,23 @@
+package com.cap.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class AccountRegistration {
+
+	
+	private String first_name;
+	private String last_name;
+	private String ssn;
+	private String dob;
+	private String email;
+	private String mobile_number;
+	private String home_address;
+	private String mailing_address;
+	private String account_type;
+	private double min_balance;
+	
+
+
+}
