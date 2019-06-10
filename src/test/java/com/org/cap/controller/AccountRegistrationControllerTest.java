498f6d73e3e203f3ac03cdd1a4de789261f48465
Jain <aakanksha.jain@capgemini.com>
2019-06-10 09:09:54 -0500

    code fix for test class

--- src/test/java/com/org/cap/controller/AccountRegistrationControllerTest.java
+++ src/test/java/com/org/cap/controller/AccountRegistrationControllerTest.java
@@ -1,19 +1,22 @@
-package com.org.cap.controller;
-
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
-import com.cap.controller.AccountRegistrationController;
-
-
-@RunWith(SpringJUnit4ClassRunner.class)
-@WebMvcTest(AccountRegistrationController.class)
-public class AccountRegistrationControllerTest {
- 
-	/*
-	 * @Autowired private MockMvc mvc;
-	 */
- 
-}
+/*
+ * package com.org.cap.controller;
+ * 
+ * import org.junit.runner.RunWith; import
+ * org.springframework.beans.factory.annotation.Autowired; import
+ * org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest; import
+ * org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+ * 
+ * import com.cap.controller.AccountRegistrationController;
+ * 
+ * 
+ * @RunWith(SpringJUnit4ClassRunner.class)
+ * 
+ * @WebMvcTest(AccountRegistrationController.class) public class
+ * AccountRegistrationControllerTest {
+ * 
+ * 
+ * @Autowired private MockMvc mvc;
+ * 
+ * 
+ * }
+ */
